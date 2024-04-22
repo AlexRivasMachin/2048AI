e0cef384e64010adb83da8423870d46f6239a14c
AlexRivasMachin <arivas028@ikasle.ehu.eus>
2024-04-22 13:19:09 +0200

    Actualizar estilos de la página principal y la barra de navegación

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './Styles/App.css'
+import NavBar from './Components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <NavBar />
+      <h1 id='logo'>
+        <span id='logoMain'>2048</span>
+        <span id='logoAi'>AI</span>
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,15 @@
+import '.././Styles/NavBar.css'
+
+function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>2048AI</li>
+                <li>Classic</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
