e38cf90b50d2af451b9573a11fcf23f0fd499067
MartinLopezDeIpina <mlopezdeipina001@ikasle.ehu.eus>
2024-04-23 15:17:54 +0200

    feat componente tema

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,8 +1,10 @@
 import '.././Styles/NavBar.css'
+import ThemeButton from './ThemeButton';
 
 function NavBar() {
     return (
         <nav>
+            <ThemeButton />
             <ul>
                 <li><a>2048AI</a></li>
                 <li><a>Classic</a></li>
