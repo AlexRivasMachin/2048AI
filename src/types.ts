24d582fd54649671d70b3b9206be88c14096af91
AlexRivasMachin <arivas028@ikasle.ehu.eus>
2024-04-30 10:07:30 +0200

    Actualizar tipos en types.ts

--- src/types.ts
+++ src/types.ts
@@ -6,11 +6,22 @@ export interface Game {
     waiting:     boolean;
     keepPlaying: boolean;
     iaPlayer:   boolean;
+    //CAMBIAR ESTO
 }
+
+const APP_STATUS = {
+    PLAYING: 'playing',
+    GAME_OVER: 'game-over',
+    GAME_WON: 'game-won',
+    WAITING : 'Waiting'
+  } as const;
+
+export type AppStatus = typeof APP_STATUS[keyof typeof APP_STATUS];
 
 export interface Grid {
     size:  number;
-    cells: Array<Array<Cell | null>>;
+    cells: Array<Array<Cell>>;
+    //MATRIZ DE 4X4    
 }
 
 export interface Cell {
