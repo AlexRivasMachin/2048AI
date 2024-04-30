d4b0f2410a2bfb2a420a41665403605dff26878c
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-04-30 10:32:57 +0200

    feat board: arreglado el tipo y logica respecto a si es ia o no. Crear celdas en modo matriz y rellenar con 0.

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -1,17 +1,12 @@
-import { MoveOptions } from "../App";
 import Board from "./Board";
 
 
 //Craeación del tablero de 2048, es oarte de Game
-export const Game = ({lastmove} : {lastmove : MoveOptions}) =>{
-
-    //handleMove  y tener un move, en el tablero que se pasa desde el game hasta el board
-
-    
+export const Game = () =>{
 
     return( 
         <>
-            <Board move={lastmove}/>
+            <Board isIA={false}/>
         </>
     )
 }
--- src/types.ts
+++ src/types.ts
@@ -1,26 +1,25 @@
+import { AppStatusType, MoveOptionsType } from "./enums";
+
 export interface Game {
-    grid:        Grid;
-    score:       number;
-    over:        boolean;
-    won:         boolean;
-    waiting:     boolean;
-    keepPlaying: boolean;
-    iaPlayer:   boolean;
-    //CAMBIAR ESTO
+  grid: Grid;
+  score: number;
+  appStatus: AppStatusType;
+  lastMove: MoveOptionsType | null;
+  iaPlayer: boolean;
 }
 
 export interface Grid {
-    size:  number;
-    cells: Array<Array<Cell>>;
-    //MATRIZ DE 4X4    
+  size: number;
+  cells: Array<Array<Cell>>;
+  //MATRIZ DE 4X4
 }
 
 export interface Cell {
-    position: Position;
-    value:    number;
+  position: Position;
+  value: number;
 }
 
 export interface Position {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 }
