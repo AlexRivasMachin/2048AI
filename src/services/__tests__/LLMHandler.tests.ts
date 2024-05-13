ad10156eaf84a42d9b671a569f03b4acb9766c31
MartinLopezDeIpina <mlopezdeipina001@ikasle.ehu.eus>
2024-05-13 18:01:17 +0200

    feat more tests, algunos fallan

--- src/services/__tests__/LLMHandler.tests.ts
+++ src/services/__tests__/LLMHandler.tests.ts
@@ -31,4 +31,60 @@ describe("LLMHandler", () => {
 
     expect([{ move: "Left" }, { move: "Right" }]).toContainEqual(result);
   });
+
+  it("should return up", async () => {
+
+    const tablero = [
+        [128, 4, 8, 2],
+        [0, 0, 4, ],
+        [128, 0, 0, 0],
+        [4, 0, 0, 0]
+    ];
+
+    const result = await handler.getBestMove(tablero);
+
+    expect([{ move: "Up" }]).toContainEqual(result);
+  });
+
+  it("should return down", async () => {
+
+    const tablero = [
+        [4, 4, 8, 2],
+        [128, 0, 4, ],
+        [0, 0, 0, 0],
+        [128, 64, 2, 0]
+    ];
+
+    const result = await handler.getBestMove(tablero);
+
+    expect([{ move: "Down" }]).toContainEqual(result);
+  });
+
+  it("should return Left", async () => {
+
+    const tablero = [
+        [4, 0, 8, 2],
+        [0, 0, 4, ],
+        [0, 0, 2, 0],
+        [128, 128, 2, 4]
+    ];
+
+    const result = await handler.getBestMove(tablero);
+
+    expect([{ move: "Left" }]).toContainEqual(result);
+  });
+
+  it("should return Right", async () => {
+
+    const tablero = [
+        [4, 0, 0, 2],
+        [4, 0, 0, 2],
+        [0, 0, 2, 2],
+        [2, 64, 128, 128]
+    ];
+
+    const result = await handler.getBestMove(tablero);
+
+    expect([{ move: "Right" }]).toContainEqual(result);
+  });
 });
