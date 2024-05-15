5e90278e58d5953c4a0ec62abd02cf2d92ef1a7a
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-05-15 11:13:43 +0200

    Revert "chore: Refactor Board component to remove unused state variables"
    This reverts commit 8d0f50dbb7820e9c5e5775d70ff0fae89c4a1529.

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -1,13 +1,26 @@
 import Board from "./Board";
 import "../Styles/Game.css";
+import { useEffect, useState } from "react";
+import { MoveOptionsType } from "../enums";
 
 //Craeación del tablero de 2048, es oarte de Game
 export const Game = () =>{
+
+    const [lastPlayerMove, setLastPlayerMove] = useState<MoveOptionsType | null>(null);
+    const [gameOver, setGameOver] = useState(false);
+    const [bestScore, setBestScore] = useState(0);
+
+    useEffect(() => {
+        if (gameOver) {
+            setGameOver(false);
+        }
+    }, [gameOver]);
+
     return( 
         <>
         <div className="boards">
-            <Board isIA={false} key={'playerGame'}/> 
-            <Board isIA={true} key={'IAGame'}/>
+            <Board isIA={false} setLastPlayerMove={setLastPlayerMove} setGameOver={setGameOver} setBestScore={setBestScore} bestScore={bestScore} key={'playerGame'}/> 
+            <Board isIA={true} lastPlayerMove={lastPlayerMove} setGameOver={setGameOver} setBestScore={setBestScore} bestScore={bestScore} key={'IAGame'}/>
         </div>
         </>
     )
