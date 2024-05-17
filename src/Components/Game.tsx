0b1bfbe370111f4c03200b096c46ae22ec6737ce
MartinLopezDeIpina <mlopezdeipina001@ikasle.ehu.eus>
2024-05-17 16:48:35 +0200

    feat: putadaMode

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -2,6 +2,8 @@ import Board from "./Board";
 import "../Styles/Game.css";
 import { useEffect, useState } from "react";
 import { MoveOptionsType, APP_STATUS, AppStatusType} from "../enums.ts";
+import { putadaMove } from "../types.ts";
+import { PunctuationSyntaxKind } from "typescript";
 
 //Craeación del tablero de 2048, es oarte de Game
 export const Game = () =>{
@@ -12,6 +14,8 @@ export const Game = () =>{
     const [bestScore, setBestScore] = useState(0);
     const [appStatus, setAppStatus] = useState<AppStatusType>(APP_STATUS.PLAYING);
     const [appStatusIA, setAppStatusIA] = useState<AppStatusType>(APP_STATUS.WAITING);
+    //Si el movimiento es el mismo, se actualizará con el counter
+    const [putadaMove, setPutadaMove] = useState<putadaMove>({move: null, counter: 0});
 
     useEffect(() => {
         if (gameOver) {
@@ -22,8 +26,8 @@ export const Game = () =>{
     return( 
         <>
         <div className="boards">
-            <Board isIA={false} lastMove={lastMove} forcedUpdate={forcedUpdate} setForcedUpdate={setForcedUpdate} setLastPlayerMove={setLastMove} setGameOver={setGameOver} setBestScore={setBestScore} bestScore={bestScore} appStatus={appStatus} setAppStatus={setAppStatus} key={gameOver ? 'palyerGame1' : 'PlayerGame2'}/> 
-            <Board isIA={true} lastMove={lastMove} forcedUpdate={forcedUpdate} setForcedUpdate={setForcedUpdate} setGameOver={setGameOver} setBestScore={setBestScore} bestScore={bestScore} appStatus={appStatusIA} setAppStatus={setAppStatusIA} setPlayerAppStatus={setAppStatus} key={gameOver ? 'IAGame1' : 'IAGame2'}/>
+            <Board isIA={false} lastMove={lastMove} forcedUpdate={forcedUpdate} setForcedUpdate={setForcedUpdate} setLastPlayerMove={setLastMove} setGameOver={setGameOver} setBestScore={setBestScore} bestScore={bestScore} appStatus={appStatus} setAppStatus={setAppStatus} key={gameOver ? 'palyerGame1' : 'PlayerGame2'} putadaMove={putadaMove} setPutadaMove={setPutadaMove}/> 
+            <Board isIA={true} lastMove={lastMove} forcedUpdate={forcedUpdate} setForcedUpdate={setForcedUpdate} setGameOver={setGameOver} setBestScore={setBestScore} bestScore={bestScore} appStatus={appStatusIA} setAppStatus={setAppStatusIA} setPlayerAppStatus={setAppStatus} key={gameOver ? 'IAGame1' : 'IAGame2'} putadaMove={putadaMove} setPutadaMove={setPutadaMove}/>
         </div>
         </>
     )
